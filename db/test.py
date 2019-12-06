dbe9fccb21dd0d931de1b2210739c5ed0747158a
Spectator <136344419@qq.com>
2019-12-06 12:10:46 +0800

    change
    change

--- db/test.py
+++ db/test.py
@@ -2,4 +2,12 @@ from db.config import *
 from db.dbgmm import *
 
 if __name__ == '__main__':
-    connection = pymysql.connect(**config)
+    conn = pymysql.connect(**config)
+    cursor = conn.cursor()
+    """
+    sql = F"insert into users (username, password, authority, nickname, phone, gender)" \
+          F" VALUE ('zjr', 'zjr954', 2, 'jerry', '11111111111', 0);"
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
+    """
